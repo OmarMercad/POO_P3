09bd559570e55791b039ae80aabc40e486660e24
OmarMercad <astaymimosa1326@gmail.com>
2023-10-31 17:16:44 -0400

    version 2023-10-31.02

---
+++ cuenta.py
@@ -0,0 +1,8 @@
+class Cuenta:
+    def __init__(self):
+        print('Construyendo el Objeto...[] '.format(self))
+        self.numero = 123
+        self.titular = "aniceto metralla"
+        self.saldo = 100.0
+        self.limite = 1000.0
+    
