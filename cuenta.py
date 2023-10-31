ece634fa339de4d557bc646e5d053be914006966
OmarMercad <astaymimosa1326@gmail.com>
2023-10-31 17:52:40 -0400

    version 2023-10-31.03

--- cuenta.py
+++ cuenta.py
@@ -1,8 +1,10 @@
 class Cuenta:
-    def __init__(self):
-        print('Construyendo el Objeto...[] '.format(self))
-        self.numero = 123
-        self.titular = "aniceto metralla"
-        self.saldo = 100.0
-        self.limite = 1000.0
+    def __init__(self, numero, titular, saldo, limite):
+        print('Construyendo el Objeto...{}'.format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+    def extracto(self):
+        print("saldo {} del titular {} ".format(self.saldo,self.titular))
     
