88eb48ea4deaf1121401d4ceef500865e61ab2b5
OmarMercad <astaymimosa1326@gmail.com>
2023-11-21 16:33:28 -0400

    version 2023-11-21.01

--- cuenta.py
+++ cuenta.py
@@ -1,19 +1,17 @@
 class Cuenta:
     def __init__(self, numero, titular, saldo, limite):
         print('Construyendo el Objeto...{}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     def extracto(self):
-        print("saldo {} del titular {}  ".format(self.saldo,self.titular))
+        print("saldo {} del titular {}  ".format(self.__saldo,self.__titular))
     
     def depositar(self,valor):
-        self.saldo += valor
+        self.__saldo += valor
         
     def retirar (self,valor):
-        if(valor<= self.saldo):
-            self.saldo -= valor
-        else:
-            print("No tiene suficiente saldo")
+        self.__saldo -= valor
+            
             
