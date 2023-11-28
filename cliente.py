494df72b5bc8260cfb2acdba2790d312f1c6f7bc
OmarMercad <astaymimosa1326@gmail.com>
2023-11-28 16:43:21 -0400

    version 2023-11-28.02

--- cliente.py
+++ cliente.py
@@ -1,13 +1,34 @@
-class Cliente:
-    def __init__(self, nombre):
-        self.__nombre = nombre
+class Cuenta:
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construyendo el objeto...{}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         
+    def extracto(self):
+        print('Saldo {} del titular {}'.format(self._saldo, self._titular))
+        
+    def depositar(self, valor):
+        self.__saldo += valor
+        
+    def retirar(self, valor):
+        self.__saldo -= valor
+        
+    def transferir(self, valor, destino):
+        self.retirar(valor)
+        destino.depositar(valor)
+        
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
     @property
-    def nombre(self):
-        print('llamando a @property nombre()')
-        return self.__nombre.title()
+    def limite(self):
+        return self.__limite
     
-    @nombre.setter
-    def nombre(self, nombre):
-        print('llamando a setter nombre()')
-        self.__nombre = nombre
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
